1d22ecf9302947bf2752ab7c02f28bb7ef5ac6d3
Tony Guglielmi <1916060+Timgur@users.noreply.github.com>
2021-12-06 14:41:11 -0500

    add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_161128) do
+ActiveRecord::Schema.define(version: 2021_12_06_193150) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2021_12_06_161128) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "passwords_digest"
+    t.string "password_digest"
   end
 
 end
