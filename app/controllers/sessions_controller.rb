cadc8117d7db508fd7ec087d6de8121f5d014055
Tony Guglielmi <1916060+Timgur@users.noreply.github.com>
2021-12-26 13:37:55 -0500

    helper methods and redirect updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in Successfully"
+      flash[:notice] = "Logged in successfully"
       redirect_to user
     else
       flash.now[:alert] = "There was something wrong with your login details"
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "logged out"
+    flash[:notice] = "logged out successfully"
     redirect_to root_path
   end
 
