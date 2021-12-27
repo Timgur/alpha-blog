126598df7ccd71741b7c8069e615df2a0bb9dae7
Tony Guglielmi <1916060+Timgur@users.noreply.github.com>
2021-12-27 11:52:49 -0500

    add admin field to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_193150) do
+ActiveRecord::Schema.define(version: 2021_12_27_164917) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_12_06_193150) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "passwords_digest"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
